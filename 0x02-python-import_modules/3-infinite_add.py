9d923d721506990b6ea642263019500d41c64b66
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-03-09 22:18:03 +0000

    infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add
+
+if __name__ == "__main__":
+    """Printing the result of the addition of all arguments"""
+
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) == 2:
+        print("{}".format(sys.argv[1]))
+    elif len(sys.argv) > 2:
+        i = 1
+        a = 0
+        while i < len(sys.argv):
+            a = a + int(sys.argv[i])
+            i = i + 1
+    print(a)
