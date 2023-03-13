0c156bcfe99e8393c1fc1d76eeb0a4970d89d2cc
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-03-13 22:44:02 +0000

    print matrix integer task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
+        l = 0
+        k = len(matrix) - 1
         for r in range(len(matrix[i])):
-            print("{}".format(matrix[i][r]), end = " ")
+            j = len(matrix[i])
+            if l ==  k:
+                print("{:d}".format(matrix[i][r]), end = '')
+            elif l < k:
+                print("{:d}".format(matrix[i][r]), end = ' ')
+            l += 1
         print()
