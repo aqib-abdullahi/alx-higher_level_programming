7695530b83f668eb5f4c13d472517953ba62bdb8
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-03-16 20:54:08 +0000

    best score task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,5 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    biggest = max(a_dictionary)
+    biggest = max(a_dictionary, key= a_dictionary.get)
     return biggest
