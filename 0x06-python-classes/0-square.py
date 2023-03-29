725545567c20eda3347bbca01eb9789932c840a8
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-03-29 21:39:55 +0000

    changed documentation for square class file added TASK 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Square Class
-This class don't do anything for the moment
+Created on the 29th of March, 2023.
 """
 
 
 class Square:
+"""Square Class
+This class don't do anything for the moment
+"""
     pass
