4d77013aa3b5bd8e74bc19303149c0ef621ea818
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-01 21:10:22 +0000

    Class with private instnce attribute correction TASK 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,8 +5,8 @@ class Square:
     """ A square class"""
 
     def __init__(self, size):
-    """Initialize square
-    Args:
-        size (int): Size of the square
-    """
+        """Initialize square
+        Args:
+            size (int): Size of the square
+        """
         self.__size = size
