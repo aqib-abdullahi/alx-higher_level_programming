e27673d797547c0c0e340cbbced10bd0c78957bf
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-01 20:55:35 +0000

    Class with private instnce attribute TASK 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on April 1, 2023.
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+    """Empty class Square that defines a square
+    Attributes:
+        size (int): Size of the square
+    """
+    self.__size = size
