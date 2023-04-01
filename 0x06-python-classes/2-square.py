a522660bd1b08041daa2d41d97088ff85fee46d7
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-01 21:22:55 +0000

    class that defines a square with optional instantiation TASK 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Created on April 1, 2023."""
+
+class Square:
+    """ A square class"""
+
+    def __init__(self, size = 0):
+        """Initialize square
+        Args:
+            size (int): Size of the square
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0') 
+        self.__size = size
