56bc96f69c8c6174e7b460542258db5d2e988bba
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-01 22:09:02 +0000

    adding getters and setters to the square class correction TASK 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,8 +23,8 @@ class Square:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
-
         self.__size = value
+
     def area(self):
         """returns the area of square"""
         return self.__size ** 2
