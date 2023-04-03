f6a332c98ca5627f9de98b09d54c2c878c1d9fc2
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-03 21:21:37 +0000

    empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" Created on 3rd April, 2023
-    A rectangle class"""
+"""
+Created on 3rd April, 2023
+A rectangle class
+"""
 
 class Rectangle:
     """Empty rectangle class"""
