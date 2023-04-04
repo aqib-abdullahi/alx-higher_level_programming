ba5110f7a338fce9f82da37b302b30d3df16c32c
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-04 13:51:06 +0000

    static method added correction

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -9,6 +9,23 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        rect_1: an instance of rectangle
+        rect_2: an instance of rectangle
+        """
+        if type(rect_1) is not Rectangle:
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if type(rect_2) is not Rectangle:
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        if rect_1 == rect_2:
+            return rect_1
+
     def __init__(self, width=0, height=0):
         """
         Init method for Rectangle
@@ -124,20 +141,3 @@ class Rectangle:
             area: The area of the rectangle
         """
         return self.__width * self.__height
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """
-        rect_1: an instance of rectangle
-        rect_2: an instance of rectangle
-        """
-        if type(rect_1) is not Rectangle:
-            raise TypeError('rect_1 must be an instance of Rectangle')
-        if type(rect_2) is not Rectangle:
-            raise TypeError('rect_2 must be an instance of Rectangle')
-        if rect_1.area() > rect_2.area():
-            return rect_1
-        if rect_1.area() < rect_2.area():
-            return rect_2
-        if rect_1 == rect_2:
-            return rect_1
