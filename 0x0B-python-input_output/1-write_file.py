19333dee8ec0f35ee5e48aadbfeb4912d0d77369
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-14 02:45:15 +0000

    write file task 1 correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Contains the function "wrtie_file"
+"""
 def write_file(filename="", text=""):
+    """returns number of characters written"""
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
