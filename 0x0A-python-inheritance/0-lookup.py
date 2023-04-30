da9f02ecdc0732657bce1c2fd00571ddfa8a711e
Aqib Abdullahi <abdullahiaqib@gmail.com>
2023-04-30 19:33:19 +0000

    Function to return list of available attributes TASK 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module has a function that returns list of available methods
+"""
 def lookup(obj):
     """
     Returns list of available  attributes and methods of an object
