b089073a9a007dda5af0fdd6c95a8c0c806f7942
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-05-18 11:26:39 -0700

    FIXED Script creates a table and makes input TASK 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table second_table in the database
 -- hbtn_0c_0 in your MySQL server and add multiples rows
 
-CREATE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
