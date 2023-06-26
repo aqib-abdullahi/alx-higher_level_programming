e784f732024e1f2328ef20274a0231ebd973e3aa
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-06-26 02:37:29 -0700

    task 2 arguments process fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 2) {
-  console.log('No Argument found');
+  console.log('No Argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
