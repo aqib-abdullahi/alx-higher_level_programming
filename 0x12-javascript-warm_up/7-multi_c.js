44d0964a899c986d4dbc9551cf96bb2af87dbfdc
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-06-26 03:06:37 -0700

    Printing with loop TASK 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let x = parseInt(process.argv[2]);
+if  (x) {
+  for (let i = 0; i < x; ++i) {
+    console.log('C is fun');
+  }
+} else
+    console.log('Mising number of occurrences');
