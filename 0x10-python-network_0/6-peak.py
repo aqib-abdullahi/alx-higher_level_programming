624fd7d0586b45c65b12f6cf7bfbae8109dcdf63
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-07-07 12:57:33 -0700

    find_peak function

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""find_peak function"""
+
+
+def find_peak(list_of_integers):
+    """
+    Return the peak in a list of integers
+    """
+    if len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
