5e59eb0a009d9ef7bab4e3e0c8c3d70543b46483
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-07-12 11:32:53 -0700

    increased documentation length

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,20 +2,16 @@
 """
 This script takes in an argument and
 displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-
+where `name` matches argument
 This time the script is safe from
 MySQL injections!
 """
-
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
+    """Access to the database and get the states
+    from the database where name matches argument..
     """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
